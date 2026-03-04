f078ff2351e5ceec7e69e7369200cb2e2acd2043
sakolwan0707 <sakolwan0707@gmail.com>
2026-03-04 23:41:31 +0700

    Add files via upload

---
+++ Lab8/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from myapp import views
+urlpatterns = {
+    path('',views.index, name='index'),
+    path('about/',views.about, name='about'),
+    path('form/',views.form, name='form'),
+    # path('contact/',views.form, name='contact')
+}
